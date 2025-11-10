716e3635993dd157ecf7900279761b71c4b3792b
RybakSvt <il4.svet@gmail.com>
2025-11-10 09:42:34 +0100

    Model Publisher

--- library/models.py
+++ library/models.py
@@ -44,5 +44,16 @@ class Book(models.Model):
     )
     genre = models.CharField(max_length=50, choices=GENRE_CHOICES, default='BIOGRAPHY')
 
+    publisher = models.ForeignKey("Publisher", on_delete=models.SET_NULL,   null=True, related_name="books")
+
+
     def __str__(self):
         return f"{self.title} --{self.author.last_name if self.author else 'NONAME'}"
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100, help_text="Name of the Publisher", verbose_name="Издательство")
+    address = models.CharField(max_length=255, null=True, blank=True, verbose_name="Адрес")
+    city = models.CharField(max_length=255, null=True, blank=True, verbose_name="Город")
+    country = models.CharField(max_length=255, verbose_name="Страна")
+
